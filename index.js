638e89eb5f50e985fdd6803836d38443b99356ce
Brandon Ros <brandonros1@gmail.com>
2020-05-17 14:17:02 -0400

    server only

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const Cp2102 = require('cp2102')
 const requestArbitrationId = parseInt(process.env.REQUEST_ARBITRATION_ID, 16)
 const replyArbitrationId = parseInt(process.env.REPLY_ARBITRATION_ID, 16)
 const deviceType = process.env.DEVICE_TYPE
-const mode = process.env.MODE
 
 const buildUsbDevice = () => {
   if (deviceType === 'gs_usb') {
@@ -20,44 +19,41 @@ const buildUsbDevice = () => {
 }
 
 const run = async () => {
-  // setup socket
-  let wss = null
-  let ws = null
-  if (mode === 'server') {
-    wss = new WebSocket.Server({
-      host: process.env.SERVER_ADDR,
-      port: process.env.SERVER_PORT
-    })
-    ws = await new Promise(resolve => wss.on('connection', resolve))
-  } else {
-    ws = new WebSocket(`ws://${process.env.SERVER_ADDR}:${process.env.SERVER_PORT}`)
-    await new Promise(resolve => ws.on('open', resolve))
-  }
-  ws.on('close', () => {
-    process.exit(1)
-  })
   // setup usb device
   const device = buildUsbDevice()
   await device.init()
-  // send incoming socket frames to USB device
-  ws.on('message', async (frame) => {
-    const arbitrationId = frame.readUInt32LE(0)
-    const data = frame.slice(4)
-    debug(`socketMessage: arbitrationId=${arbitrationId.toString(16)} data=${data.toString('hex')}`)
-    await device.sendCanFrame(arbitrationId, data)
+  // setup socket
+  let wss = null
+  let connections = []
+  wss = new WebSocket.Server({
+    host: process.env.SERVER_ADDR,
+    port: process.env.SERVER_PORT
+  })
+  wss.on('connection', (ws) => {
+    // send incoming socket frames to USB device
+    ws.on('message', async (frame) => {
+      const arbitrationId = frame.readUInt32LE(0)
+      const data = frame.slice(4)
+      debug(`socketMessage: arbitrationId=${arbitrationId.toString(16)} data=${data.toString('hex')}`)
+      await device.sendCanFrame(arbitrationId, data)
+    })
+    // register connection in array
+    connections.push(ws)
   })
   // send incoming USB device frames to socket
   device.on('frame', (frame) => {
     const arbitrationId = frame.readUInt32LE(0)
     const data = frame.slice(4)
-    const shouldProxyFrame = (mode === 'client' && arbitrationId === replyArbitrationId) ||
-      (mode === 'server' && arbitrationId === requestArbitrationId)
+    const shouldProxyFrame = arbitrationId === requestArbitrationId
     if (!shouldProxyFrame) {
       debug(`dropping frame; arbitrationId = ${arbitrationId.toString(16)}`)
       return
     }
     debug(`deviceFrame: arbitrationId=${arbitrationId.toString(16)} data=${data.toString('hex')}`)
-    ws.send(frame)
+    // send to connections
+    for (let i = 0; i < connections.length; ++i) {
+      connections[i].send(frame)
+    }
   })
   // start USB device receive loop
   await device.recv()
